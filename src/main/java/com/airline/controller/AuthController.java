11817d8edde90703322425f24ac5b8e82b56b64e
binh113 <quocbinh230597@gmail.com>
2023-06-06 19:43:48 +0700

    clone dev

--- src/main/java/com/airline/controller/AuthController.java
+++ src/main/java/com/airline/controller/AuthController.java
@@ -13,14 +13,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin ("*")
+@CrossOrigin("*")
 @RestController
-@RequestMapping ("/api/auth")
+@RequestMapping("/api/auth")
 public class AuthController {
     @Autowired
     private UserService userService;
     @Autowired
     private AuthService authService;
+
     @PostMapping("/login")
     public ResponseEntity<?> login(@Valid @RequestBody LoginRequest loginRequest) {
         try {
@@ -30,6 +31,7 @@ public class AuthController {
             return new ResponseEntity<>("this Account is not valid", HttpStatus.BAD_REQUEST);
         }
     }
+
     @PostMapping("/register")
     public ResponseEntity<?> create(@Validated @RequestBody UserDtoCreateRequest userDtoCreateRequest) {
         checkEmailPassword checkEmailPassword = userService.save(userDtoCreateRequest);
@@ -37,6 +39,7 @@ public class AuthController {
             return new ResponseEntity<>(null, HttpStatus.OK);
         } else return new ResponseEntity<>(checkEmailPassword, HttpStatus.BAD_REQUEST);
     }
+
     @GetMapping
     public ResponseEntity<?> findUserByAccount(@Valid @RequestParam String account) {
         try {
